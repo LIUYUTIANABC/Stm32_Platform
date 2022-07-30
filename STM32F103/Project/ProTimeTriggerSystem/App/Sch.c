7b22bf477947e830e64d36e013863f7c47a48a91
LiuYutian <1428456495@qq.com>
2022-07-30 23:36:50 +0800

    Add Sch “Refresh” function in interrupt of TIM4

--- STM32F103/Project/ProTimeTriggerSystem/App/Sch.c
+++ STM32F103/Project/ProTimeTriggerSystem/App/Sch.c
@@ -1,5 +1,6 @@
 #include "Sch.h"
 #include "time.h"
+#include "led.h"
 
 //------------------------------------------------------------------------
 //- 数据类型 sTask 和常数 SCH_MAX_TASKS 创建任务队列，被调度器引用
@@ -40,4 +41,43 @@ void SCH_Init_TIM4(void)
 void SCH_Delete_Task(tByte i)
 {
     ;
+}
+
+/*******************************************************************************
+* 函 数 名         : TIM4_IRQHandler
+* 函数功能		   : TIM4中断函数, Sch 的刷新函数
+* 输    入         : 无
+* 输    出         : 无
+*******************************************************************************/
+void TIM4_IRQHandler(void)
+{
+    tByte Index;
+
+    if(TIM_GetITStatus(TIM4,TIM_IT_Update))
+    {
+        // 计算单位为“时标”，只是我们用了 1ms
+        for ( Index = 0; Index < SCH_MAX_TASKS; Index++)
+        {
+            // 检测这里是否有任务
+            if (SCH_tasks_G[Index].pTask)
+            {
+                if (SCH_tasks_G[Index].Delay == 0)
+                {
+                    // 任务需要运行
+                    SCH_tasks_G[Index].RunMe += 1;  // "RunMe" 标准加1
+                    if (SCH_tasks_G[Index].Period)
+                    {
+                        // 调度周期性的任务再次运行
+                        SCH_tasks_G[Index].Delay = SCH_tasks_G[Index].Period;
+                    }
+                }
+                else
+                {
+                    // 还没有准备好运行，延迟减1
+                    SCH_tasks_G[Index].Delay -= 1;
+                }
+            }
+        }
+    }
+    TIM_ClearITPendingBit(TIM4,TIM_IT_Update);
 }
