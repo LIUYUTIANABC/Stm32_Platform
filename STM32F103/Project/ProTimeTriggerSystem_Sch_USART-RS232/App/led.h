22dc2d1664d883b6cfd2c3d7fbc753ecbd422943
rick.liu <rick.liu@dgevolt.com>
2022-08-19 16:06:54 +0800

    Add a project for TimeTrigger-RS232

---
+++ STM32F103/Project/ProTimeTriggerSystem_Sch_USART-RS232/App/led.h
@@ -0,0 +1,13 @@
+#ifndef _LED_H
+#define _LED_H
+
+#include "system.h"
+
+#define LED_BIT_GPIOC_PIN14 PCout(14)
+#define LED_BIT_GPIOC_PIN13 PCout(13)
+
+void LedInit(void);
+void LedFlash(void);
+void LedFlashUpdate(void);
+
+#endif
